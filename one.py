5c2de7bbe35d2cb0edebc7d745ab15702c8486cc
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2020-04-28 12:06:54 -0500

    modified one.py

--- one.py
+++ one.py
@@ -1,6 +1,9 @@
 """ Pallendrome Checker 
-    For this first problem fill in the 
+    For this first problem fix the function
+    so that if the input text is a pallendrome, it returns True.
+    And if not, it returns false.
 
+    Try running it on some samples to make sure it works
 """
 
 
@@ -17,3 +20,19 @@ text0 = "My foot is a hamburger"
 text1 = "Go hang a salami Im a lasagna hog"
 text2 = "She sells sea shells by the sea shore"
 text3 = "race car"
+
+# Test it like this
+print(tester(text0,False))
+print(tester(text1,True))
+
+
+def tester(text,expected_result):
+  actual_result = checkisPallendrom(text)
+
+  print_text = "The text was: " + str(text0)
+  if expected_result == actual_result:
+    print_text += "and the function was correct"
+  else:
+    print_text += "and the function was not correct"
+  return print_text 
+
