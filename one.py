2cab468e5b0143380d8e8c40aaa4d4b4f4f32d3c
AJM10565 <ajm10565@gmail.com>
2020-04-28 17:13:20 +0000

    fixed one.py tester

--- one.py
+++ one.py
@@ -15,24 +15,28 @@ def checkisPallendrome(text):
   return result
 
 
-# Try it on these to see if it works?
-text0 = "My foot is a hamburger"
-text1 = "Go hang a salami Im a lasagna hog"
-text2 = "She sells sea shells by the sea shore"
-text3 = "race car"
-
-# Test it like this
-print(tester(text0,False))
-print(tester(text1,True))
 
 
 def tester(text,expected_result):
-  actual_result = checkisPallendrom(text)
+  actual_result = checkisPallendrome(text)
 
-  print_text = "The text was: " + str(text0)
+  print_text = "The text was: " + str(text0) + " | "
   if expected_result == actual_result:
-    print_text += "and the function was correct"
+    print_text += "Correct: " + str(expected_result) + " == " + str(actual_result)
   else:
-    print_text += "and the function was not correct"
+    print_text += "Incorrect: " + str(expected_result) + " != " + str(actual_result)
   return print_text 
+
+
+if __name__ == "__main__":
+    # Try it on these to see if it works?
+  text0 = "My foot is a hamburger"
+  text1 = "Go hang a salami Im a lasagna hog"
+  text2 = "She sells sea shells by the sea shore"
+  text3 = "race car"
+
+  # Test it like this
+  print(tester(text0,False))
+  print(tester(text1,True))
+    
 
