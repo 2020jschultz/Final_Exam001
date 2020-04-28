4bb9d42f055724e8e1085ecd3e068d946d4e66da
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2020-04-28 12:58:44 -0500

    added two.py fixed one.py

--- one.py
+++ one.py
@@ -7,36 +7,29 @@
 """
 
 
-
 def checkisPallendrome(text):
+    result = False
+    return result
 
 
-  result = False
-  return result
+def tester(text, expected_result):
+    actual_result = checkisPallendrome(text)
 
-
-
-
-def tester(text,expected_result):
-  actual_result = checkisPallendrome(text)
-
-  print_text = "The text was: " + str(text0) + " | "
-  if expected_result == actual_result:
-    print_text += "Correct: " + str(expected_result) + " == " + str(actual_result)
-  else:
-    print_text += "Incorrect: " + str(expected_result) + " != " + str(actual_result)
-  return print_text 
+    print_text = "The text was: " + str(text0) + " | "
+    if expected_result == actual_result:
+        print_text += "Correct: " + str(expected_result) + " == " + str(actual_result)
+    else:
+        print_text += "Incorrect: " + str(expected_result) + " != " + str(actual_result)
+    return print_text
 
 
 if __name__ == "__main__":
-  # Try it on these to see if it works?
-  text0 = "My foot is a hamburger"
-  text1 = "Go hang a salami Im a lasagna hog"
-  text2 = "She sells sea shells by the sea shore"
-  text3 = "race car"
+    # Try it on these to see if it works?
+    text0 = "My foot is a hamburger"
+    text1 = "Go hang a salami Im a lasagna hog"
+    text2 = "She sells sea shells by the sea shore"
+    text3 = "race car"
 
-  # Test it like this
-  print(tester(text0,False))
-  print(tester(text1,True))
-    
-
+    # Test it like this
+    print(tester(text0, False))
+    print(tester(text1, True))
