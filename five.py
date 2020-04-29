d48c8f209cd12bad8685e7ccd50bf2b1f8d0e7c8
AJM10565 <32456394+AJM10565@users.noreply.github.com>
2020-04-28 21:33:38 -0500

    Final Exam is good to go

---
+++ five.py
@@ -0,0 +1,22 @@
+"""  
+    For this problem you will modify a function that creates a password
+    the password needs to include features from the input list
+    that contains information about the person.
+    Passwords need to have 3 random special characters
+    contain jumbled strings from the input
+    include 2-4 numbers
+    be of lenght 18-36 characters
+    NOTE: 0 points if when you run the code again, the password is similar.
+          Every time you run the code you should get a unique string. 
+"""
+import random
+
+def passwordGenerator(personal_list):
+    password = n00b1234
+    return password
+
+
+
+if __name__ == "__main__":
+    mylist = [12,"BOB",-2,"Amy",["David","Jona","Timmothy"],"Burt"]
+    print(passwordGenerator(mylist))
