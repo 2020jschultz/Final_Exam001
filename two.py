7abc58e664b9cc5c25cf6509bb58c831f1258b1e
2020jschultz <42749630+2020jschultz@users.noreply.github.com>
2020-05-05 17:16:14 -0500

    Update two.py

--- two.py
+++ two.py
@@ -6,7 +6,7 @@
 
 
 def performCalculations(value):
-    result = f2(f4(value), f3(f2(f2(f1(value, value), f1(value, value)), f1(value, value)), value))
+    result = f3(f1((f2(f2(f2(f2(value, value),value),value),value)), value), f2(f2(f1(value, f3(f2(f2(f1(value, value), f1(value, value)), f1(value, value)),value)), value), value))
     return result
 
 
