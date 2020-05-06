b89bff26278cee3cfce7221cd51af946ffb972cd
2020jschultz <42749630+2020jschultz@users.noreply.github.com>
2020-05-05 22:43:01 -0500

    Update four.py

--- four.py
+++ four.py
@@ -16,8 +16,65 @@
 """
 
 
-def fun():
-    pass
+def questionOne():
+    inputF = open("four.txt", 'r')
+    outputF = open("solution4.txt", 'w')
+
+    for line in inputF:
+        outputString = ""
+        for i in line:
+            if i.isupper():
+                outputString = outputString + i
+            else:
+                pass
+        outputF.write(outputString)
+
+    inputF.close()
+    outputF.close()
+
+
+def questionTwo():
+    inputF = open("four.txt", 'r')
+
+    numbers = '0123456789'
+
+    finalNum3 = 0
+
+    for fileLines in inputF:
+        for text in fileLines:
+            if text in numbers:
+                # print(i)
+                finalNum3 = finalNum3 + int(text)
+    outputString = ""
+
+    with open("four.txt") as fp:
+        for i, line in enumerate(fp):
+            if i >= 10:
+
+                for aLine in line:
+                    outputString = outputString + aLine.replace("|", " ")
+                break
+
+    counter = 0
+
+    if "|" not in outputString:
+        for i in outputString:
+            counter = counter + 1
+            if counter >= finalNum3:
+                print("test")
+                print(i)
+                break
+
+    inputF.close()
+    
+    
+def part3():
+    
+
+
+if __name__ == "__main__":
+    # questionOne()
+    questionTwo()
 
 if __name__ == "__main__":
     fun()
